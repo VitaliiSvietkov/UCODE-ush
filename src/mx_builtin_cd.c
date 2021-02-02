8e96b2f76998bae8f0f607f6979f2b095a9870be
Vitalii Svietkov <vsvietkov@c1r5p3.khpi.ucode-connect.study>
2021-02-02 13:15:11 +0200

    Fixed cd but it is not finished

--- src/mx_builtin_cd.c
+++ src/mx_builtin_cd.c
@@ -1,7 +1,52 @@
 #include "../inc/ush.h"
 
 void mx_builtin_cd(char *path) {
-    if (chdir(path) < 0) {
-        perror("ush");
+    int status;
+    if (path[0] == '/') {
+        status = chdir(path);
+        if (status < 0) {
+            perror("ush");
+            return;
+        }
+        status = setenv("OLDPWD", getenv("PWD"), 1);
+        status = setenv("PWD", path, 1);
+    }
+    else {
+        // Check if the path is a symlink
+        char buf[PATH_MAX];
+        ssize_t nbytes = readlink(path, buf, PATH_MAX);
+        if (nbytes > 0) {
+            status = chdir(path);
+            if (status < 0) {
+                perror("ush");
+                return;
+            }
+            status = setenv("OLDPWD", getenv("PWD"), 1);
+            char *tmp_path = mx_strjoin(getenv("OLDPWD"), path);
+            status = setenv("PWD", tmp_path, 1);
+            free(tmp_path);
+            return;
+        }
+
+        char real_buf[PATH_MAX];
+        char *res = realpath(path, real_buf);
+        if (!res)
+            free(res);
+
+        status = chdir(path);
+        if (status < 0) {
+            perror("ush");
+            return;
+        }
+
+        /*char *cwd = (char *)malloc(INT_MAX);
+        getcwd(cwd, INT_MAX);
+        char *ptr = realpath(cwd, NULL);
+        if (ptr == NULL)
+            printf("Error\n");*/
+        
+        status = setenv("OLDPWD", getenv("PWD"), 1);
+        status = setenv("PWD", real_buf, 1);
+        //free(cwd);
     }
 }
--- src/mx_pwd_flags_set.c
+++ src/mx_pwd_flags_set.c
@@ -4,11 +4,18 @@ void mx_pwd_flags_set(t_flags_pwd *data, char **flags) {
     if (flags[1] != NULL) {
         if (flags[1][0] == '-') {
             for (int i = 1; i < mx_strlen(flags[1]); i++) {
-                if (flags[1][i] == 'L') {
-		    data->using_L = true;
-		    continue;
-		}
-		if (flags[1][i] != 'L' && flags[1][i] != 'P') {
+
+                switch (flags[1][i])
+                {
+                case 'P':
+                    data->using_P = true;
+                    data->using_L = false;
+                    break;
+                case 'L':
+                    data->using_L = true;
+                    data->using_P = false;
+                    break;
+                default:
                     mx_printerr("ush: pwd: -");
                     char c = flags[1][i];
                     mx_printerr(&c);
@@ -17,6 +24,7 @@ void mx_pwd_flags_set(t_flags_pwd *data, char **flags) {
                     errno = 1;
                     break;
                 }
+
             }
         }
     }
