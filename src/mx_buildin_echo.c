99dbc6919bbc95869ee0293e5c47abfb4e538169
NogaKazaha <oleg.savich2001@gmail.com>
2021-02-22 15:57:20 +0200

    Started echo with flags

---
+++ src/mx_buildin_echo.c
@@ -0,0 +1,26 @@
+#include "../inc/ush.h"
+
+void mx_buildin_echo(t_flags_echo *flags, char **data) {
+    if (data[1] != NULL) {
+        int i = 1;
+        char *str = NULL;
+        str = mx_strnew(256);
+        while(data[i] != NULL) {
+            if(!mx_strcmp(">", data[i]) && data[i+1] != NULL) {
+                printf("Write to file");
+            }
+            else {
+                mx_strcat(str, data[i]);
+                if(data[i+1] != NULL) {
+                    mx_strcat(str, " ");
+                }
+            }
+            i++;
+        }
+        mx_printstr(str);
+        mx_printchar('\n');
+    }
+    if(flags->using_N) {
+
+    }
+}
