d1a2abea3546434586a9c799e242622de032a6fc
Oleh Savich <osavich@c1r12p10.khpi.ucode-connect.study>
2021-02-25 18:39:22 +0200

    ...

--- src/mx_builtin_echo.c
+++ src/mx_builtin_echo.c
@@ -1,6 +1,6 @@
 #include "../inc/ush.h"
 
-char *trim (char *s)
+static char *trim (char *s)
 {
     int i;
     while (mx_isspace (*s)) s++;   // skip left side white spaces
@@ -8,6 +8,26 @@ char *trim (char *s)
     s[i + 1] = '\0';
     return s;
     //printf ("%s\n", s);
+}
+
+static char *removeSpaces(char *str)
+{
+    if (*str == '\0') return str;
+
+    char *back = str;
+    char *front = str + 1;
+    /* In place removal of duplicate spaces*/
+    while(*front != '\0')
+    {
+        if (*front != ' ' || *back != ' ')    // highlight
+            *(++back) = *front;
+        front++;
+    }
+
+    /* After above step add end of string*/
+    *(back + 1) = '\0';
+
+    return str;
 }
 
 int mx_builtin_echo(t_flags_echo *flags, char **data) {
@@ -40,7 +60,9 @@ int mx_builtin_echo(t_flags_echo *flags, char **data) {
         bool parants = false;
         for (int i = 0; str[i]; i++) {
             if (str[i] == '"' || str[i] == '\'') {
-                str[i] = ' ';
+                for (int j = i; str[j]; j++) {
+                    str[j] = str[j + 1];
+                }
                 count++;
                 parants = true;
             }
@@ -54,6 +76,7 @@ int mx_builtin_echo(t_flags_echo *flags, char **data) {
         }
         if(count % 2 == 0 || count == 0) {
             if(isWrite) {
+                removeSpaces(str);
                 char *newstr = trim(str);
                 mx_printstr(newstr);
             }
@@ -91,7 +114,9 @@ int mx_builtin_echo(t_flags_echo *flags, char **data) {
         bool parants = false;
         for (int i = 0; str[i]; i++) {
             if (str[i] == '"' || str[i] == '\'') {
-                str[i] = ' ';
+                for (int j = i; str[j]; j++) {
+                    str[j] = str[j + 1];
+                }
                 count++;
                 parants = true;
             }
@@ -105,6 +130,7 @@ int mx_builtin_echo(t_flags_echo *flags, char **data) {
         }
         if(count % 2 == 0 || count == 0) { 
             if (isWrite) {
+                removeSpaces(str);
                 char *newstr = trim(str);
                 mx_printstr(newstr);
                 mx_printchar('\n');
