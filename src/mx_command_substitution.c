f61c65bfd5629846dcaa5234ca29887ba1abdfda
VitaliiSvietkov <a1vitalii.sv@gmail.com>
2021-02-25 23:57:52 +0200

    Implemented  and  substitution

--- src/mx_command_substitution.c
+++ src/mx_command_substitution.c
@@ -71,11 +71,127 @@ int mx_command_substitution(char **str) {
             ptr = mx_strchr(data, '`');
         }
     }
-
-    ptr = mx_strchr(data, '$');
+    
+    ptr = strchr(data, '$');
     if (!ptr)
         return 0;
-    
+
+    // Loop for replacing simple $COMMAND
+    while (ptr != NULL) {
+        if (*(ptr + 1) == '{' || *(ptr + 1) == '(') {
+            ptr = strchr(++ptr, '$');
+            continue;
+        }
+        char *tmp_ptr = ptr;
+
+        if (*(ptr + 1) != '(' && *(ptr + 1) != '{') {
+            int ptr_len = mx_strlen(ptr) + 2;
+            char *str_to_replace = mx_strnew(ptr_len);
+            char *str_var = mx_strnew(ptr_len);
+
+            str_to_replace[0] = *tmp_ptr;
+            tmp_ptr++;
+            for (int i = 1, j = 0; *tmp_ptr != '\0' && *tmp_ptr != ' ' && *tmp_ptr != '$'; i++, j++) {
+                str_to_replace[i + 1] = '\0';
+                str_var[j + 1] = '\0';
+
+                str_to_replace[i] = *tmp_ptr;
+                str_var[j] = *tmp_ptr;
+                if (str_var[0] == '?')
+                    break;
+                tmp_ptr++;
+            }
+
+            if (str_var[0] == '?') {
+                char *status = mx_itoa(t_global.exit_status);
+                *str = rep_substr(*str, str_to_replace, status);
+                free(status);
+                free(str_to_replace);
+                free(str_var);
+                ptr = strchr(*str, '$');
+                continue;
+            }
+            
+            char *env_val = getenv(str_var);
+            char *env = NULL;
+            if (env_val == NULL)
+                env = mx_strdup("\0");
+            else 
+                env = mx_strdup(env_val);
+
+            *str = rep_substr(*str, str_to_replace, env);
+
+            free(env);
+            free(str_to_replace);
+            free(str_var);
+        }
+
+        ptr = strchr(*str, '$');
+    }
+
+    ptr = strchr(*str, '$');
+    if (!ptr)
+        return 0;
+
+    // Loop for other simple substitutions like ${COMMAND}
+    while (ptr != NULL) {
+        if (*(ptr + 1) == '(') {
+            ptr = strchr(++ptr, '$');
+            continue;
+        }
+        char *tmp_ptr = ptr;
+        if (*(ptr + 1) == '{') {
+            int ptr_len = mx_strlen(ptr) + 2;
+            char *str_to_replace = mx_strnew(ptr_len);
+            char *str_var = mx_strnew(ptr_len);
+
+            str_to_replace[0] = *tmp_ptr;
+            str_to_replace[1] = *(tmp_ptr + 1);
+            tmp_ptr += 2;
+            for (int i = 2, j = 0; *tmp_ptr != '}'; i++, j++) {
+                str_to_replace[i + 1] = '\0';
+                str_var[j + 1] = '\0';
+
+                str_to_replace[i] = *tmp_ptr;
+                if (*tmp_ptr != '}')
+                    str_var[j] = *tmp_ptr;
+                if (*tmp_ptr == ' ' || *tmp_ptr == '\0') {
+                    mx_printerr("ush: bad substitution\n");
+                    free(str_to_replace);
+                    free(str_var);
+                    t_global.exit_status = 1;
+                    return -1;
+                }
+                tmp_ptr++;
+                if (*tmp_ptr == '}')
+                    str_to_replace[i + 1] = *tmp_ptr;
+            }
+
+            char *env_val = getenv(str_var);
+            char *env = NULL;
+            if (env_val == NULL)
+                env = mx_strdup("\0");
+            else 
+                env = mx_strdup(env_val);
+
+            *str = rep_substr(*str, str_to_replace, env);
+
+            free(env);
+            free(str_to_replace);
+            free(str_var);
+        }
+        ptr = strchr(*str, '$');
+        if (ptr == tmp_ptr)
+            ptr = strchr(++ptr, '$');
+    }
+
+
+
+
+
+
+
+    /*
     while (ptr != NULL) {
     
         char *tmp_ptr = ptr;
@@ -215,6 +331,7 @@ int mx_command_substitution(char **str) {
         }
         ptr = mx_strchr(data, '$');
     }
+    */
     return 0;
 }
 
@@ -222,13 +339,16 @@ static char *rep_substr(char *str, char *substr, char *new_str) {
     int index = mx_get_substr_index(str, substr);
     if (index == -1)
         return NULL;
-    int len = mx_strlen(str + mx_strlen(new_str));
+    int new_len = mx_strlen(new_str);
+    int len = mx_strlen(str) + mx_strlen(new_str);
     char *res = mx_strnew(len);
     mx_memcpy(res, str, index);
-    mx_memcpy(res + index, new_str, mx_strlen(new_str));
-    mx_memcpy(res + index + mx_strlen(new_str), 
-        str + index + 1, len - index - 1 - mx_strlen(new_str));
+    if (new_len > 0)
+        mx_memcpy(res + index, new_str, new_len);
+    mx_memcpy(res + index + new_len, 
+        str + index + mx_strlen(substr), len - index - mx_strlen(substr) - new_len);
     free(str);
+
     return res;
 }
 
